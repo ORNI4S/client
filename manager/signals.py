9d3fe38ee80abc58bceec2644bd1bcc1618295b1
ali <alifreeguy@gmail.com>
2023-04-14 22:52:06 +0330

    fix bug remove account

--- manager/signals.py
+++ manager/signals.py
@@ -6,8 +6,11 @@ from user import models as md
 import datetime
 import requests
 import json
-
-
+from django.db.models.signals import pre_delete
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from django.contrib.admin.models import LogEntry
+from django.contrib.contenttypes.models import ContentType
 
 
 
@@ -40,18 +43,13 @@ def add_server(sender  , **kwargs) :
 
 
 
-
-from django.db.models.signals import pre_delete
-
-# @receiver(pre_delete, sender=md.UserAccounts)
-# def log_deleted_question(sender, instance, using, **kwargs):
-    
-#     # if instance.user.is_superuser : 
-#     #     print('e')
-#         res =requests.get(f'{instance.link}/kill/{instance.pid}')
-#         instance.user.userdata.account_used -= 1
-#         instance.user.userdata.save()
-
-
-
-
+@receiver(pre_delete, sender=md.UserAccounts)
+def mymodel_post_delete(sender, instance, using ,**kwargs):
+    content_type = ContentType.objects.get_for_model(md.UserAccounts)
+    logs = LogEntry.objects.filter(content_type=content_type, object_id=instance.id)
+    if logs.exists():
+            res =requests.get(f'{instance.link}/kill/{instance.pid}')
+            instance.user.userdata.account_used -= 1
+            instance.user.userdata.save()
+    else:
+        print('#')
